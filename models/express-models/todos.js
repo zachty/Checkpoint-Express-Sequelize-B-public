427fda688a6ed2602ab4c85b9dfdcc4506eb120f
Zach Byerley <zachbyerley@gmail.com>
2022-08-13 12:28:44 -0400

    finished todos

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -18,21 +18,34 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if (!task.complete) task.complete = false;
+    let current = tasks[name];
+    if (current) {
+      tasks[name] = [...current, task];
+    } else {
+      tasks[name] = [task];
+    }
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
     // marks a task complete
+    tasks[name][idx].complete = true;
   },
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name] = tasks[name].filter(
+      (task) => tasks[name].indexOf(task) !== idx
+    );
   },
 };
