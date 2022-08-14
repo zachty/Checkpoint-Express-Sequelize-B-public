5b26cfb364df6a5a8267de56be69d9ade45ed75c
Zach Byerley <zachbyerley@gmail.com>
2022-08-14 15:04:55 -0400

    submission for deadline

--- models/sequelize-models/index.js
+++ models/sequelize-models/index.js
@@ -5,6 +5,7 @@ const Sequelize = require('sequelize');
 
 //---------VVVV---------  your code below  ---------VVV----------
 
+//Task model
 const Task = db.define('Task', {
   name: {
     type: Sequelize.STRING,
@@ -20,19 +21,77 @@ const Task = db.define('Task', {
   due: Sequelize.DATE,
 });
 
-const Owner = db.define('Owner', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+//Model methods
+Task.clearCompleted = async function () {
+  //select all tasks where cpmpleted is true and remove them
+  await Task.destroy({
+    where: {
+      complete: true,
+    },
+  });
+};
+
+Task.completeAll = async function () {
+  await Task.update({ complete: true }, { where: { complete: false } });
+};
+
+//Task instance methods
+Task.prototype.getTimeRemaining = function () {
+  const due = this.due - Date.now();
+  return due ? due : Infinity;
+  // return this.due - Date.now() || Infinity; //short circuit
+};
+
+Task.prototype.isOverdue = function () {
+  return this.getTimeRemaining() < 0 && !this.complete;
+};
+
+Task.prototype.assignOwner = function (owner) {
+  return this.setOwner(owner);
+};
+
+//Owner model
+const Owner = db.define(
+  'Owner',
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
   },
-});
+  {
+    hooks: {
+      beforeDestroy: (owner) => {
+        if (owner.name === 'Grace Hopper') throw new Error('Not allowed!');
+      },
+    },
+  }
+);
 
+//Owner model methods
+Owner.getOwnersAndTasks = function () {
+  //returns a promise of all owners and tasks
+  return Owner.findAll({
+    include: {
+      model: Task,
+    },
+  });
+};
+
+//Owner instance methods
+Owner.prototype.getIncompleteTasks = function () {
+  return this.getTasks({ where: { complete: false } });
+};
+
+//Owner instance hook
+//Owner.beforeDestroy(owner => {})
+
+//relations that create magic methods
 Task.belongsTo(Owner);
 Owner.hasMany(Task);
-
 
 //---------^^^---------  your code above  ---------^^^----------
 
